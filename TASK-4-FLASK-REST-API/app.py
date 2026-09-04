6f0e32523e9e87cc0825d4716bea72291d277e53
Vivek <vivekcraj321@gmail.com>
2026-09-04 08:13:19 +0530

    Complete Task 4 Flask REST API

--- TASK-4-FLASK-REST-API/app.py
+++ TASK-4-FLASK-REST-API/app.py
@@ -7,19 +7,47 @@ tasks_list = [
     {"id": 2, "title": "Build REST API"}
 ]
 
+
 @app.route("/")
 def home():
     return "Flask REST API is running!"
 
-@app.route("/tasks")
+
+@app.route("/tasks", methods=["GET"])
 def get_tasks():
     return {"tasks": tasks_list}
-        
 
-@app.route("/tasks/<int:task_id>")
+
+@app.route("/tasks/<int:task_id>", methods=["GET"])
 def get_task(task_id):
     for task in tasks_list:
         if task["id"] == task_id:
+            return task
+
+    return {"error": "Task not found"}, 404
+
+
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    data = request.get_json()
+
+    new_task = {
+        "id": len(tasks_list) + 1,
+        "title": data["title"]
+    }
+
+    tasks_list.append(new_task)
+
+    return new_task, 201
+
+
+@app.route("/tasks/<int:task_id>", methods=["PUT"])
+def update_task(task_id):
+    data = request.get_json()
+
+    for task in tasks_list:
+        if task["id"] == task_id:
+            task["title"] = data["title"]
             return task
 
     return {"error": "Task not found"}, 404
@@ -31,17 +59,6 @@ def delete_task(task_id):
         if task["id"] == task_id:
             tasks_list.remove(task)
             return {"message": "Task deleted successfully"}
-
-    return {"error": "Task not found"}, 404
-
-@app.route("/tasks/<int:task_id>", methods=["PUT"])
-def update_task(task_id):
-    data = request.get_json()
-
-    for task in tasks_list:
-        if task["id"] == task_id:
-            task["title"] = data["title"]
-            return task
 
     return {"error": "Task not found"}, 404
 
